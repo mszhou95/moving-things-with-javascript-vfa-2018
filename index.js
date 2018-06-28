c44491c226c4d7d5261996549b7462c4349ab471
Michele Zhou <michele.zhou@gmail.com>
2018-06-28 06:34:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const app = "I don't do much."
+
+
+      var dodger = document.getElementById('dodger')
+      dodger.style.backgroundColor = "#FF69B4"
+      dodger.style.bottom = '0px'
+      
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
